6f86d1f5bdf6a1149b0dbd834c8c83cf6e4b7180
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-29 17:31:33 -0300

    Rendering Dummy Post Data

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import classes from "post-item.module.css";
+import classes from "./post-item.module.css";
 
 function PostItem(props) {
   const { title, image, excerpt, date, slug } = props.post;
@@ -12,14 +12,21 @@ function PostItem(props) {
     year: "numeric",
   });
 
-  const imagePath = `/images/posts${slug}/${image}`;
+  const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={linkPath}>
         <a>
           <div className={classes.image}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout="responsive"
+            />
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>

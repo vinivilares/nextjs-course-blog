3f20d0881b23ba216f8cd5ac25a0b5a8d5c8b790
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-29 17:12:45 -0300

    Adding a Post Grid and Post Items

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Logo from "./logo";
 import classes from "./main-navigation.module.css";
 
-function MainNavigation(props) {
+function MainNavigation() {
   return (
     <header className={classes.header}>
       <Link href="/">

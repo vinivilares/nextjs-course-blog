2e0f754baf55655f87d8cb58939044d3ac78c5cf
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-07-04 14:07:13 -0300

    Adding Enviroments Variables

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,15 +24,16 @@ export default async function handler(req, res) {
     };
 
     let client;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.she17xy.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`
 
     try {
-      client = await MongoClient.connect(process.env.MONGODB_URL);
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database" });
       return;
     }
 
-    const db = client.db("nextjs-course-blog");
+    const db = client.db();
 
     try {
       const result = await db.collection("messages").insertOne(newMessage);
